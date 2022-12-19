b34edc29c9a41a9d5c5449a7a20c8b488efa562a
lucocozz <lucocozz@student.42.fr>
2022-12-19 17:08:05 +0100

    c'est noel

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # ft_ping
 
 man ping: http://www.man-linux-magique.net/man8/ping.html
+man ip: http://manpagesfr.free.fr/man/man7/ip.7.html
+man socket: http://manpagesfr.free.fr/man/man7/socket.7.html
+man socket raw: http://manpagesfr.free.fr/man/man7/raw.7.html
 
 protocol ICMP: https://fr.wikipedia.org/wiki/Internet_Control_Message_Protocol
  - rfc: http://abcdrfc.free.fr/rfc-vf/rfc0792.html
-
-implementation: http://gauss.ececs.uc.edu/Courses/c4029/labs/ICMP-PingReplies.pdf
--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 19:39:11 by user42            #+#    #+#             */
-/*   Updated: 2022/12/12 20:57:06 by user42           ###   ########.fr       */
+/*   Updated: 2022/12/14 17:01:38 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,32 @@
 int	init_socket(t_options options)
 {
 	int		raw_socket;
-	int		type;
-	uid_t	uid = getuid();
 
-	type = (uid == 0 ? SOCK_RAW : SOCK_DGRAM);
-	raw_socket = socket(PF_INET, type, IPPROTO_ICMP);
+	raw_socket = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
 	if (raw_socket == -1)
-		return (-1);
-	if (setuid(uid) == -1)
 		return (-1);
 	(void)options;
 	// setsockopt(raw_socket, );
 	return (raw_socket);
 }
 
+void	resolve_service_name(const char *service_name, struct addrinfo **peer_address)
+{
+	struct addrinfo	hints = {0};
+	char			msg[256] = {0};
+
+	hints.ai_socktype = SOCK_RAW;
+	if (getaddrinfo(service_name, NULL, &hints, peer_address) < 0) {
+		sprintf(msg, "%s: Name or service not known", service_name);
+		fatal(EXIT_FAILURE, msg);
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	t_options		options;
 	int				raw_socket;
+	struct addrinfo	*peer_address;
 
 	if (argc == 1)
 		fatal(EXIT_ERROR, MSG_MISSING_DEST);
@@ -42,6 +50,8 @@ int	main(int argc, char **argv)
 	raw_socket = init_socket(options);
 	if (raw_socket == -1)
 		fatal(EXIT_FAILURE, strerror(errno));
+	resolve_service_name(options.destination, &peer_address);
+	freeaddrinfo(peer_address);
 	close(raw_socket);
 	return (EXIT_SUCCESS);
 }
